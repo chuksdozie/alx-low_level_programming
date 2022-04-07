ab5647a1be4c9b41fd30f40e0b87660cae1dd0e8
chuks dozie <chuksdozie48@gmail.com>
2022-04-07 16:50:20 -0700

    multilingual task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *main - entry point
  *Return 0 after printing the function
 */
-int main(void)
+int main(int argc, char *argv[])
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
