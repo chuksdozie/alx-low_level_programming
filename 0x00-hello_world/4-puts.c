d590e016e72e853aa4e68ca2da4cbc7eb4afcb02
chuks dozie <chuksdozie48@gmail.com>
2022-04-07 17:14:04 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *main - entry point
  *Return 0 after printing the function
 */
-int main(void){
+int main(void) /*jd dsgjsd*/
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
