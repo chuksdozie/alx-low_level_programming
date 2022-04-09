01301872884fd0fd8e2492fecc299605e7df76e2
chuks dozie <chuksdozie48@gmail.com>
2022-04-08 20:21:16 -0700

    advanced task 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int c, i, k, j;
 
-	while (p < 58)
+	for (c = 48; c <= 57; c++)
 	{
-		while (q < 58)
+		for (i = 48; i <= 57; i++)
 		{
-			while (y < 58)
+			for (k = 48; k <= 57; k++)
 			{
-				while (x < 58)
+				for (j = 48; j <= 57; j++)
 				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
 					{
-						putchar(',');
+						putchar(c);
+						putchar(i);
 						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
 					}
-					x++;
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
 				}
-				y++;
-				x = 48;
 			}
-			q++;
-			q = p;
-			x = q + 1;
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
