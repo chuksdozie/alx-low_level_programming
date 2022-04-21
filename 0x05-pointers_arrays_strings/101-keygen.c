29a6b7161cfbd952291541d7d4e7e887fdb6bb88
chuks dozie <chuksdozie48@gmail.com>
2022-04-20 20:17:15 -0700

    advanced task 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,17 +9,17 @@
  */
 int main(void)
 {
-	int pass, sum;
+        int pass, sum;
 
-	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
-	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
-	}
-	printf("%c", 2772 - sum);
+        srand(time(NULL));
+        sum = 0;
+        while (sum <= 2645)
+        {
+                pass = (rand() % 128);
+                sum += pass;
+                printf("%c", pass);
+        }
+        printf("%c", 2772 - sum);
 
-	return (0);
+        return (0);
 }
