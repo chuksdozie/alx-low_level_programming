d6800227bad47797369f5bea12bf700ae625c481
chuks dozie <chuksdozie48@gmail.com>
2022-04-29 02:15:28 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -53,3 +53,24 @@ if (s1[a] != '\0')
 		if (s2[b + 1] == '*')
 			return (checker(s1, s2, a, b + 1, b));
 		else if (s2[b + 1] == s1[a])
+			return (checker(s1, s2, a, b + 1, b));
+		else if (s1[a + 1] != s2[b + 1])
+			return (checker(s1, s2, a + 1, b, b));
+		else if (s1[a + 1] == s2[b + 1])
+			return (checker(s1, s2, a + 1, b + 1, b));
+	}
+	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
+		return (checker(s1, s2, a + 1, b + 1, wildUsed));
+
+	if (wildUsed == -1)
+		return (0);
+
+	return (checker(s1, s2, a, wildUsed, wildUsed));
+
+}
+if (s2[b] != '\0')
+	return (checkLast(s2, b));
+
+return (1);
+
+}
